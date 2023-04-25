ec7c0acd07719705618fa047adb8bd33b612bcfd
EniCela <enicela20@gmail.com>
2023-04-25 18:31:45 +0200

    login from and register

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        if($request->isMethod('POST')){
+            $data = $request->input();
+
+            $userDetails = User::where('email', $data['email'])->first();
+
+            if( !$userDetails || !password_verify($data['password'], $userDetails->password)){
+                return response()->json([
+                    'status' => 400,
+                    'message' => "Invalid credentials.!"
+                ]);
+            }
+            // else{
+            //     if($userDetails->roli == 1){
+            //         $role = 'admin';
+            //         $token = $userDetails->createToken($userDetails->email . '_AdminToken', ['server:admin'])->plainTextToken;
+            //     }
+            //     else{
+            //         $role = '';
+                    $token = $userDetails->createToken($userDetails->email . '_Token')->plainTextToken;
+                // }
+
+                return response()->json([
+                    'status' => 200,
+                    "username" => $userDetails->username,
+                    'token' => $token,
+                    'message' => "Login Successfully!"
+                ]);
+            //}
+        }
+    }
+
+    public function register(Request $request)
+    {
+        if($request->isMethod('POST')){
+
+            $data = $request->input();
+
+            $userEmail = User::where('email', $data['email'])->first();
+            if(!$userEmail){
+
+                $val = $request->validate([
+                    'email' => 'required|:rfc,dns|unique:users,email',
+                    'password' => ['required', Password::min(8)->letters()->numbers()->symbols()->mixedCase()],
+                    'username' => 'required',
+                ]);
+
+                if($val){
+
+                    $user = new User;
+                    $user->email = $data['email'];
+                    $user->password = Hash::make($data['password']);
+                    $user->username = $data['username'];
+
+                    $user->save();
+
+                    $token = $user->createToken($user->email . '_Token')->plainTextToken;
+
+                    return response()->json([
+                        'status' => 200,
+                        'message' => "Registration was Successful!",
+                        "username" => $data['username'],
+                        'token' => $token,
+                    ]);
+                }
+                else{
+                    return response()->json([
+                        'status' => 400,
+                        'message' => "Something went wrong."
+                    ]);
+                }
+            }
+            else{
+                return response()->json([
+                    'status' => 400,
+                    'message' => "Email already exists.",
+                ]);
+            }
+
+        }
+    }
+
+}
---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // $admin = User::create([
+        //     'name'=>'admin',
+        //     'email'=>'eni@gmail.com',
+        //     'password'=>bcrypt('eni'),
+        // ]);
+
+        // $users = factory(User::class, 5)-> create();
+
+    }
+}
